372bc0e23d1e3004a9ac9e17841285847572b521
Lonny <gmljacobson@gmail.com>
2014-02-17 17:40:45 -0700

    Created the basic package layout

---
+++ src/main/java/liquibase/ext/spatial/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Liquibase Spatial provides spatial extensions to Liquibase.
+ */
+package liquibase.ext.spatial;
---
+++ src/main/java/liquibase/ext/spatial/xml/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the Liquibase Spatial XSD.
+ */
+package liquibase.ext.spatial.xml;
