d4e21135f18ea21201a1c03a4de936e418e7dc43
Lonny <gmljacobson@gmail.com>
2014-06-15 21:23:39 -0600

    Added a check for "geometry_columns" prior to querying for the existance of a column in that table.

--- src/main/java/liquibase/ext/spatial/utils/GeometryColumnsUtils.java
+++ src/main/java/liquibase/ext/spatial/utils/GeometryColumnsUtils.java
@@ -7,7 +7,10 @@ import java.sql.Statement;
 import liquibase.database.Database;
 import liquibase.database.DatabaseConnection;
 import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
 import liquibase.exception.UnexpectedLiquibaseException;
+import liquibase.snapshot.SnapshotGeneratorFactory;
+import liquibase.structure.core.Table;
 
 /**
  * <code>GeometryColumnsUtils</code> provides useful methods for checking the
@@ -22,8 +25,8 @@ public class GeometryColumnsUtils {
    }
 
    /**
-    * Determines if the given table is in <code>GEOMETRY_COLUMNS</code> and, therefore, has a
-    * geometry column.
+    * Determines if the given table is in <code>GEOMETRY_COLUMNS</code> and,
+    * therefore, has a geometry column.
     * 
     * @param database
     *           the database to query.
@@ -33,26 +36,46 @@ public class GeometryColumnsUtils {
     *           the table name to check.
     * @return <code>true</code> if the table has a geometry column.
     */
-   public static boolean hasGeometryColumn(final Database database, final String schemaName,
-         final String tableName) {
+   public static boolean hasGeometryColumn(final Database database,
+         final String schemaName, final String tableName) {
       boolean isSpatialColumn;
       Statement jdbcStatement = null;
       try {
-         final String query = "SELECT * FROM geometry_columns WHERE f_table_schema = '"
-               + (schemaName == null ? database.getDefaultSchemaName() : schemaName)
-               + "' AND f_table_name = '" + tableName + "'";
-         final DatabaseConnection databaseConnection = database.getConnection();
-         final JdbcConnection jdbcConnection = (JdbcConnection) databaseConnection;
-         jdbcStatement = jdbcConnection.getUnderlyingConnection().createStatement();
-         final ResultSet rs = jdbcStatement.executeQuery(query);
-         isSpatialColumn = rs.next();
+         Table geometryColumns = new Table();
+         geometryColumns.setSchema(database.getDefaultCatalogName(),
+               database.getDefaultSchemaName());
+         geometryColumns.setName("geometry_columns");
+         if (!SnapshotGeneratorFactory.getInstance().has(geometryColumns,
+               database)) {
+            System.out.println("geometry_columns is not found");
+            isSpatialColumn = false;
+         } else {
+            final String query = "SELECT * FROM geometry_columns WHERE f_table_schema = '"
+                  + (schemaName == null ? database.getDefaultSchemaName()
+                        : schemaName)
+                  + "' AND f_table_name = '"
+                  + tableName
+                  + "'";
+            final DatabaseConnection databaseConnection = database
+                  .getConnection();
+            final JdbcConnection jdbcConnection = (JdbcConnection) databaseConnection;
+            jdbcStatement = jdbcConnection.getUnderlyingConnection()
+                  .createStatement();
+            final ResultSet rs = jdbcStatement.executeQuery(query);
+            isSpatialColumn = rs.next();
+         }
       } catch (final SQLException e) {
          throw new UnexpectedLiquibaseException(
                "Failed to determine if the table has a geometry column", e);
+      } catch (final LiquibaseException e) {
+         throw new UnexpectedLiquibaseException(
+               "Failed to determine if the geometry_columns table exists", e);
       } finally {
-         try {
-            jdbcStatement.close();
-         } catch (final SQLException ignore) {
+         if (jdbcStatement != null) {
+            try {
+               jdbcStatement.close();
+            } catch (final SQLException ignore) {
+            }
          }
       }
       return isSpatialColumn;
@@ -71,23 +94,29 @@ public class GeometryColumnsUtils {
     *           the column name.
     * @return <code>true</code> if the column is a geometry column.
     */
-   public static boolean isGeometryColumn(final Database database, final String schemaName,
-         final String tableName, final String columnName) {
+   public static boolean isGeometryColumn(final Database database,
+         final String schemaName, final String tableName,
+         final String columnName) {
       boolean isSpatialColumn;
       Statement jdbcStatement = null;
       try {
          final String query = "SELECT * FROM geometry_columns WHERE f_table_schema = '"
-               + (schemaName == null ? database.getDefaultSchemaName() : schemaName)
-               + "' AND f_table_name = '" + tableName + "' AND upper(f_geometry_column) = '"
+               + (schemaName == null ? database.getDefaultSchemaName()
+                     : schemaName)
+               + "' AND f_table_name = '"
+               + tableName
+               + "' AND upper(f_geometry_column) = '"
                + columnName.toUpperCase() + "'";
          final DatabaseConnection databaseConnection = database.getConnection();
          final JdbcConnection jdbcConnection = (JdbcConnection) databaseConnection;
-         jdbcStatement = jdbcConnection.getUnderlyingConnection().createStatement();
+         jdbcStatement = jdbcConnection.getUnderlyingConnection()
+               .createStatement();
          final ResultSet rs = jdbcStatement.executeQuery(query);
          isSpatialColumn = rs.next();
       } catch (final SQLException e) {
          throw new UnexpectedLiquibaseException(
-               "Failed to determine if the column to be dropped is a geometry column", e);
+               "Failed to determine if the column to be dropped is a geometry column",
+               e);
       } finally {
          try {
             jdbcStatement.close();
@@ -96,5 +125,4 @@ public class GeometryColumnsUtils {
       }
       return isSpatialColumn;
    }
-
 }
